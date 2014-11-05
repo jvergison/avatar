3513125569b2e86a2582fdba0fc32f854e921b8d
bschelstraete <b.schelstraete@outlook.be>
2014-11-05 16:34:07 +0100

    removed initScript - niet meer nodig-

--- nbproject/background.js
+++ nbproject/background.js
@@ -4,7 +4,8 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
         return;
     }
     var match = 'https://www.google.com/calendar/render';
-    if (tab.url.substring(0, match.length) === match)
+    var match2 = 'https://www.google.com/calendar/b/1/render';
+    if (tab.url.substring(0, match.length) === match || tab.url.substring(0, match.length) === match2)
     {
         chrome.tabs.executeScript(null, { file: "scripts/jquery-1.11.1.min.js" }, function() {
             chrome.tabs.executeScript(null, { file: "main.js" });
--- nbproject/main.js
+++ nbproject/main.js
@@ -36,5 +36,4 @@ function initButton() {
 }
 
 function openStatistics(){
-    //not yet implemented
 }
