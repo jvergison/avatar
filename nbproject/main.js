a393f999740f86c4593292842c3afedbe805899b
bschelstraete <b.schelstraete@outlook.be>
2014-11-05 16:23:38 +0100

    removed initScript - niet meer nodig-

--- nbproject/main.js
+++ nbproject/main.js
@@ -4,15 +4,7 @@
 })();
 
 function init() {
-    initScript();
     initButton();
-}
-
-function initScript() {
-    var script = document.createElement('script');
-    script.src = 'https://code.jquery.com/jquery-1.11.0.min.js';
-    script.type = 'text/javascript';
-    document.getElementsByTagName('head')[0].appendChild(script);
 }
 
 function initButton() {
