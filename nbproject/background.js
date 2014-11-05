9b4f5323d28520ce57e4622af2334a03809c02de
bschelstraete <b.schelstraete@outlook.be>
2014-11-05 15:00:20 +0100

    JQUERY added

--- nbproject/background.js
+++ nbproject/background.js
@@ -6,6 +6,9 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     var match = 'https://www.google.com/calendar/render';
     if (tab.url.substring(0, match.length) === match)
     {
-        chrome.tabs.executeScript(tabId, { file: 'main.js' });
+        chrome.tabs.executeScript(null, { file: "scripts/jquery-1.11.1.min.js" }, function() {
+            chrome.tabs.executeScript(null, { file: "main.js" });
+        });
     }
 });
+
--- nbproject/main.js
+++ nbproject/main.js
@@ -1,31 +1,13 @@
-(function () {
-    var id = 'ppdahdklecfhdhfdkhalcddcidlgmeon';
-    var myAudio = new Audio('chrome-extension://' + id + '/today.wav');
-    myAudio.load();
-    var myImg = new Image();
-    myImg.src = 'chrome-extension://' + id + '/neildiamond.png';
-    myImg.style.position = 'absolute';
-    myImg.style.display = 'none';
-    document.body.appendChild(myImg);
 
-    setInterval(function () {
+(function(){
+    var id = "pagknpjhappmknlejlnemfdjndgodcbn";
+    init();
+    $("#sidebar").hide();
+})();
 
-        var buttons = document.getElementsByClassName('date-nav-today');
-        var i;
-        for (i = 0; (i < buttons.length); i++) {
-            var button = buttons[i];
-            var inner = button.getElementsByClassName('goog-imageless-button-content')[0];
-            if (!inner.getAttribute('data-' + id + '-set')) {
-                inner.addEventListener('click', function () {
-                    myAudio.currentTime = 0;
-                    myAudio.play();
-                    startAnimation();
-                    return true;
-                });
-                inner.setAttribute('data-' + id + '-set', 1);
-            }
-        },
-        100
-        )
-        ;
-    })();
+function init(){
+    var script = document.createElement('script');
+    script.src = 'https://code.jquery.com/jquery-1.11.0.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
